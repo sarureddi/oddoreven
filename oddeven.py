a1c9c034f76269a9b690c09f45b70b5e97b86a5f
saru <sarumathiayyappan@gmail.com>
2019-06-06 14:49:47 +0530

    Create oddeven.py

---
+++ oddeven.py
@@ -0,0 +1,8 @@
+n1=int(input("enter the value1:"))
+n2=int(input("enter the value2:"))
+sum=n1+n2
+print("The sum of two numbers is:",sum)
+if(sum%2==0):
+  print("The sum is even")
+else:
+  print("The sum is odd")
